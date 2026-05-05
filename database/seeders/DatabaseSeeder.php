2eb493b26754503d4622efb5547ab000bbbe251b
Nico Deblauwe <nico@bluepundit.eu>
2026-05-05 11:38:41 +0200

    FEATURE :sparkles: Add Category model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Article;
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,7 +18,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        Category::factory(4)->create();
         Article::factory(20)->create();
-
     }
 }
