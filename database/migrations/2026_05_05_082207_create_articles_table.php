71ac94213ad2eddaf8c9c683aa2b98634eebed37
Nico Deblauwe <nico@bluepundit.eu>
2026-05-05 10:54:02 +0200

    FEATURE :sparkles: Add Article Model

---
+++ database/migrations/2026_05_05_082207_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+
+            // Model properties
+            $table->string('title');
+            $table->text('content');
+            $table->string('image_path')->nullable();
+            $table->timestamp('published_at')->nullable();
+
+            // Model relation properties
+            $table->foreignId('author_id')->nullable();
+            $table->foreignId('category_id')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
