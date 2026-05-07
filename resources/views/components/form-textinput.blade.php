0de2b283a2be90b9c421d5ba57b7b6dc634c2b2f
Nico Deblauwe <nico@bluepundit.eu>
2026-05-07 14:28:33 +0200

    DESIGN :art: Add empty line after component

--- resources/views/components/form-textinput.blade.php
+++ resources/views/components/form-textinput.blade.php
@@ -5,6 +5,8 @@
     'value' => ''
 ])
 
-<label for="{{$name}}">{{$label}}</label>
-<input type="text" name="{{$name}}" placeholder="{{$placeholder}}" value="{{old($name,$value)}}">
-@error($name) <div style="color: red"> {{ $message }} </div> @enderror
+<div>
+    <label for="{{$name}}">{{$label}}</label>
+    <input type="text" name="{{$name}}" placeholder="{{$placeholder}}" value="{{old($name,$value)}}">
+    @error($name) <div style="color: red"> {{ $message }} </div> @enderror
+</div>
