531a52af08fd527a75c48d88865db8f19ed33fee
Nico Deblauwe <nico@bluepundit.eu>
2026-05-07 12:11:59 +0200

    FEATURE :sparkles: Add Faq model +table migration + fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Article;
 use App\Models\Category;
+use App\Models\Faq;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create();
         Category::factory(4)->create();
+        Faq::factory(10)->create();
         Article::factory(20)->create();
     }
 }
