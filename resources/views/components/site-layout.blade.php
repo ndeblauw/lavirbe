00a7a90520161295b40dd6a5018043d7f2a04276
Nico Deblauwe <nico@bluepundit.eu>
2026-05-12 11:00:02 +0200

    DESIGN :art: Display name of authenticated user or login links in header

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -1,6 +1,17 @@
 <div>
     <div>
         Dit wordt mijn header
+
+        @auth
+            | <span style="color: blue"> {{auth()->user()->name}} </span>
+
+
+        @else
+            <a href="{{route('login')}}">Login</a>
+            or
+            <a href="{{route('register')}}">Register</a>
+        @endauth
+
         <hr/>
     </div>
 
