947a6ca42a0defcabad97942abc006f35ce66ded
Nico Deblauwe <nico@bluepundit.eu>
2026-05-13 10:29:20 +0200

    DESIGN :art: Make mavigation stylish

--- resources/views/components/site-layout-navigation.blade.php
+++ resources/views/components/site-layout-navigation.blade.php
@@ -1,9 +1,21 @@
+@php
+    $menu_items = [
+        ['name' => 'Categorieën', 'url' => 'categories'],
+        ['name' => 'Faq', 'url' => 'faqs'],
+        ['name' => 'Artikels', 'url' => 'articles'],
+    ];
+
+    $current_route = request()->route()->uri();
+@endphp
+
 <nav class="mt-16 mb-24 text-right">
 
     <ul class="flex gap-4 justify-end">
-        <li><a href="/categories">Categorieën</a></li>
-        <li><a href="/faqs">Faq</a></li>
-        <li><a href="/articles">Artikels</a></li>
+        @foreach($menu_items as $item)
+            <li class="border-b @if($current_route == $item['url']) border-black @else border-transparent @endif hover:border-dotted hover:border-black px-2 py-1">
+                <a href="/{{$item['url']}}">{{$item['name']}}</a>
+            </li>
+        @endforeach
     </ul>
 
 
